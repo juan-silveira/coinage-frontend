8a9ee515ab4638f8c44f7080f6497dd2d93a220f
Juan Silveira <juansilveira@gmail.com>
2023-09-28 09:38:20 -0300

    feat: hidding balance when visibility button is pressed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -918,56 +918,6 @@
     }
   }
 
-  // Create chart instance
-  var chart = am4core.create("chartdiv", am4charts.PieChart);
-
-  // Add data
-  chart.data = [{
-    "products": "FTR",
-    "share": 501.9,
-    "color": am4core.color("#FCD535")
-  }, {
-    "products": "IMB",
-    "share": 301.9,
-    "color": am4core.color("#0f8f62")
-  }, {
-    "products": "LOC",
-    "share": 201.1
-  }, {
-    "products": "JUD",
-    "share": 50
-  }];
-
-  // Add and configure Series
-  var pieSeries = chart.series.push(new am4charts.PieSeries());
-  pieSeries.dataFields.value = "share";
-  pieSeries.dataFields.category = "products";
-  pieSeries.slices.template.propertyFields.fill = "color";
-  pieSeries.innerRadius = am4core.percent(75);
-  pieSeries.ticks.template.disabled = true;
-  pieSeries.labels.template.disabled = true;
-
-  var rgm = new am4core.LinearGradientModifier();
-  rgm.brightnesses.push(0, - 0.4);
-  pieSeries.slices.template.fillModifier = rgm;
-
-  var rgm2 = new am4core.LinearGradientModifier();
-  rgm2.brightnesses.push(0, - 0.4);
-
-  pieSeries.slices.template.strokeModifier = rgm2;
-  pieSeries.slices.template.strokeOpacity = 1;
-  pieSeries.slices.template.strokeWidth = 1;
-  chart.legend = new am4charts.Legend();
-  chart.legend.position = "bottom";
-
-  pieSeries.slices.template.events.on("validated", function (event) {
-    var gradient = event.target.fillModifier.gradient
-    gradient.rotation = event.target.middleAngle + 90;
-
-    var gradient2 = event.target.strokeModifier.gradient
-    gradient2.rotation = event.target.middleAngle + 90;
-  })
-
   // change theme
   var ThemeOn = false;
   $('#changeThemeLight').on('click', function (e) {
--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -12,27 +12,90 @@ let balanceVisibility = true;
 const handleVisibility = () => {
     const eye = document.getElementById("eye");
     if (balanceVisibility == true) {
-        document.getElementById("balance-hidden").style.display = "block";
-        document.getElementById("balance").style.display = "none";
-        document.getElementById("chart-container").style.display = "none";
+        const hidden = document.querySelectorAll(".balance-hidden");
+        for (var i = 0; i < hidden.length; i++) {
+            hidden[i].style.display = "block";
+        }
+        const visible = document.querySelectorAll(".balance-visible");
+        for (var i = 0; i < visible.length; i++) {
+            visible[i].style.display = "none";
+        }
         eye.classList.remove("ion-ios-eye");
         eye.classList.add("ion-ios-eye-off");
         balanceVisibility = false;
     } else {
-        document.getElementById("balance-hidden").style.display = "none";
-        document.getElementById("balance").style.display = "block";
-        document.getElementById("chart-container").style.display = "block";
+        const hidden = document.querySelectorAll(".balance-hidden");
+        for (var i = 0; i < hidden.length; i++) {
+            hidden[i].style.display = "none";
+        }
+        const visible = document.querySelectorAll(".balance-visible");
+        for (var i = 0; i < visible.length; i++) {
+            visible[i].style.display = "block";
+        }
         eye.classList.remove("ion-ios-eye-off");
         eye.classList.add("ion-ios-eye");
         balanceVisibility = true;
     }
 }
 
-window.addEventListener('load', (event) => {
+// Create chart instance
+var chart = am4core.create("chartdiv", am4charts.PieChart);
+
+// Add data
+chart.data = [{
+    "products": "FTR",
+    "share": 501.9,
+    "color": am4core.color("#FCD535")
+}, {
+    "products": "IMB",
+    "share": 301.9,
+    "color": am4core.color("#0f8f62")
+}, {
+    "products": "LOC",
+    "share": 201.1
+}, {
+    "products": "JUD",
+    "share": 50
+}];
+
+const handleLoadPieChart = () => {
+    // Add and configure Series
+    var pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "share";
+    pieSeries.dataFields.category = "products";
+    pieSeries.slices.template.propertyFields.fill = "color";
+    pieSeries.innerRadius = am4core.percent(75);
+    pieSeries.ticks.template.disabled = true;
+    pieSeries.labels.template.disabled = true;
+
+    var rgm = new am4core.LinearGradientModifier();
+    rgm.brightnesses.push(0, - 0.4);
+    pieSeries.slices.template.fillModifier = rgm;
+
+    var rgm2 = new am4core.LinearGradientModifier();
+    rgm2.brightnesses.push(0, - 0.4);
+    pieSeries.slices.template.tooltipText = "{category}: {value.percent.formatNumber('#.#')}%";
+    pieSeries.slices.template.strokeModifier = rgm2;
+    pieSeries.slices.template.strokeOpacity = 1;
+    pieSeries.slices.template.strokeWidth = 1;
+    chart.legend = new am4charts.Legend();
+    chart.legend.position = "bottom";
+
+    pieSeries.slices.template.events.on("validated", function (event) {
+        var gradient = event.target.fillModifier.gradient
+        gradient.rotation = event.target.middleAngle + 90;
+
+        var gradient2 = event.target.strokeModifier.gradient
+        gradient2.rotation = event.target.middleAngle + 90;
+    })
+}
+
+window.addEventListener('load', () => {
     const portfolio = document.querySelector('#portfolio');
     const wallet = document.querySelector('#wallet');
     portfolio.classList.toggle('table-responsive', window.matchMedia('(max-width:500px)').matches);
     wallet.classList.toggle('table-responsive', window.matchMedia('(max-width:500px)').matches);
+    handleLoadPieChart();
 });
 
 window.addEventListener('resize', () => {
